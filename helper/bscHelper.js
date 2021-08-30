ea42963bc132f724c797b7f93b970c20af29ce63
rajkumaryadavlive <rajkumar.live.mp@gmail.com>
2021-08-30 11:31:25 +0530

    Abrand source code

---
+++ helper/bscHelper.js
@@ -0,0 +1,21 @@
+// const web3 = require('web3');
+// const Tx = require('ethereumjs-tx').Transaction;
+// const Common = require('ethereumjs-common');
+
+// const web3js = new web3(
+//     new web3.providers.HttpProvider(
+//       "https://data-seed-prebsc-1-s1.binance.org:8545"
+//     )
+// );
+// const createWalletHelper = async () => {
+//     console.log(web3.eth)
+//     let newData = await web3js.eth.accounts.create();
+    
+//     if(newData){
+//         return newData.privateKey;
+//     }
+// };
+
+// module.exports = {
+//     createWalletHelper
+// }
